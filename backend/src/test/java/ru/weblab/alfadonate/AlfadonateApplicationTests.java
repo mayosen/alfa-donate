3e5175080f7af68288c725ec23565187e0f7cbbd
mayosen <gare577@gmail.com>
2022-11-12 00:00:55 +0300

    Init spring application

---
+++ backend/src/test/java/ru/weblab/alfadonate/AlfadonateApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.weblab.alfadonate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfadonateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
