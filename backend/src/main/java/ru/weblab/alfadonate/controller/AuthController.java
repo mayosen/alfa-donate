761aab6a1c36a0c7690343045fbf9ff5d851b4ad
mayosen <gare577@gmail.com>
2022-11-13 12:55:56 +0300

    Set CORS

--- backend/src/main/java/ru/weblab/alfadonate/controller/AuthController.java
+++ backend/src/main/java/ru/weblab/alfadonate/controller/AuthController.java
@@ -1,9 +1,11 @@
 package ru.weblab.alfadonate.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+@CrossOrigin(value = "http://localhost:5001", maxAge = 900)
 @RestController
 public class AuthController {
     @PostMapping("/register")
