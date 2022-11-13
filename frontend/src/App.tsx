fd83a25330923bf95d9f6db409dd76a8054aaaa3
elithium4 <elizaveta.elithium@yandex.ru>
2022-11-13 14:42:24 +0300

    Fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing/>}/>
+        <Route path="/" element={[<Landing/>]}/>
       </Routes>
     </BrowserRouter>
   );
