25cacfb3b8918044ef4b3a0f337f096d9d3c6854
elithium4 <elizaveta.elithium@yandex.ru>
2022-11-13 14:21:06 +0300

    Routing startes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
 import Landing from './pages/landing/landing';
-import Analytics from './pages/analytics/analytics';
-
-import { IDonate, IFund } from './api/popup';
-import BlankFund from './pages/blank fund/blankFund';
-
+import { Router, Route, Routes } from 'react-router';
 
 function App() {
   return (
-    <Analytics></Analytics>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing/>}/>
+      </Routes>
+    </Router>
   );
 }
 
